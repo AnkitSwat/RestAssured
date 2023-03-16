4f0aa318fc798e3193dd69ea901f12e978410568
AnkitSwat <toankitsaraswat@gmail.com>
2023-03-16 20:45:21 +0530

    uploaded work of day 1 part 3

--- src/test/java/Day1/HTTPRequests.java
+++ src/test/java/Day1/HTTPRequests.java
@@ -5,10 +5,14 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 
 public class HTTPRequests {
 
-	@Test
+	int id;
+	
+	@Test(priority=1)
 	void getUsers() {
 		
 		given()
@@ -20,5 +24,81 @@ public class HTTPRequests {
 			.statusCode(200)
 			.body("page",equalTo(2))
 			.log().all();
+	}
+	
+	
+	//@Test(priority=2)
+	void createUser()
+	{
+		HashMap data=new HashMap();
+		data.put("name","Luffy");
+		data.put("job","Captain");
+		
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+			
+		.when()
+			.post("https://reqres.in/api/users")
+			
+		.then()
+			.statusCode(201)
+			.log().all();
+	
+	}
+
+	@Test(priority=3)
+	void createUserGetId()
+	{
+		HashMap data=new HashMap();
+		data.put("name","Luffy");
+		data.put("job","Captain");
+		
+		
+		id=given()
+			.contentType("application/json")
+			.body(data)
+			
+		.when()
+			.post("https://reqres.in/api/users")
+			.jsonPath().getInt("id");
+		
+	
+	}
+
+	@Test(priority=4,dependsOnMethods= {"createUserGetId"})
+	void updateUser()
+	{
+		HashMap data=new HashMap();
+		data.put("name","Zoro");
+		data.put("job","Swordsman");
+		
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+			
+		.when()
+			.put("https://reqres.in/api/users/"+id)
+			
+		.then()
+		.statusCode(200)
+		.log().all();
+	}
+	
+	@Test(priority=5,dependsOnMethods= {"createUserGetId"})
+	void deleteUser() {
+		
+		given()
+		
+		.when()
+			.delete("https://reqres.in/api/users/"+id)
+			
+		.then()
+			.statusCode(204)
+			.log().all();
+		
+		
 	}
 }
